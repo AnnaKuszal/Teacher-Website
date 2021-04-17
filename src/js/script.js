b88c190a2bfa4c8588dc0a7421ff45ee5b7b3541
Anna Kuszal <anna.kuszal@o2.pl>
2021-04-17 20:21:36 +0200

    Add js code for “Skills” – opening modals

--- src/js/script.js
+++ src/js/script.js
@@ -24,3 +24,35 @@ rollup.addEventListener('click', function() {
     rollup.innerHTML = 'ROZWIŃ';
   }
 });
+
+/* Modal for images */
+
+const modal = document.getElementById('modal');
+const modalContentWrapper = document.getElementById('modal-content-wrapper');
+const images = document.querySelectorAll('.skills .skills-box img');
+
+for (let image of images) {
+  let src = image.getAttribute('data-src');
+
+  image.addEventListener('click', function() {
+    let modalImg = document.createElement('img');
+    modalImg.classList.add('modal-content');
+    modalImg.setAttribute('src', src);
+    modal.style.display = 'block';
+    modalContentWrapper.appendChild(modalImg);
+  });
+}
+
+const span = document.getElementsByClassName('m-close')[0];
+
+span.onclick = function() {
+  let modalImgActive = document.querySelector('.modal-content-wrapper img');
+  modalImgActive.remove();
+  modal.style.display = 'none';
+};
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = 'none';
+  }
+};
