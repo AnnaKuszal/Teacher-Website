2d615ede95c7f09779e73dbacd055b7f55725029
Anna Kuszal <anna.kuszal@o2.pl>
2021-04-17 20:47:44 +0200

    Add js for “Materials” – activating buttons&links

--- src/js/script.js
+++ src/js/script.js
@@ -56,3 +56,84 @@ window.onclick = function(event) {
     modal.style.display = 'none';
   }
 };
+
+/* Activating side menu links */
+
+const sideMenuLinks = document.getElementById('ul02').querySelectorAll('a');
+const openBtnMenu = document.getElementById('openbtnMenu');
+const closeBtnMenu = document.getElementById('closebtnMenu');
+const sideMenu = document.getElementById('ul02');
+const resourcesMenu = document.getElementById('resources-menu');
+const resMenuWidthVal = resourcesMenu.clientWidth;
+
+const sideBtnClickHandler = function(event) {
+  event.preventDefault();
+  const clickedElement = this;
+  const activeChapters = document.querySelectorAll('.chapter.active');
+
+  if (bodyWidthVal < 576 && resMenuWidthVal > 170) {
+    sideMenu.classList.remove('show');
+    openBtnMenu.style.display = 'block';
+    closeBtnMenu.style.display = 'none';
+  }
+
+  for (let sideMenuLink of sideMenuLinks) {
+    sideMenuLink.classList.remove('active');
+  }
+
+  clickedElement.classList.add('active');
+  const idFromMenuLink = clickedElement.getAttribute('href').replace('#', '');
+  window.location.hash = '#materials/' + idFromMenuLink;
+
+  // Activate the right chapter
+  for (let activeChapter of activeChapters) {
+    activeChapter.classList.remove('active');
+  }
+
+  const chapterSelector = clickedElement.getAttribute('href');
+  const targetChapter = document.querySelector(`#${chapterSelector}`);
+  targetChapter.classList.add('active');
+
+  //get activeNavLink
+  const activeNavLink = document.querySelector('a.active');
+
+  const activeNavLinkSectionSelector = activeNavLink.getAttribute('href');
+
+  if (activeNavLinkSectionSelector !== '#materials') {
+    for (let navLink of navLinks) {
+      navLink.classList.remove('active');
+      const navLinkSectionSelector = navLink.getAttribute('href');
+      if (navLinkSectionSelector == '#materials') {
+        navLink.classList.add('active');
+      }
+    }
+  }
+};
+
+for (let sideMenuLink of sideMenuLinks) {
+  sideMenuLink.addEventListener('click', sideBtnClickHandler);
+}
+
+/* Side menu & buttons - show/hide */
+
+openBtnMenu.addEventListener('click', function() {
+  sideMenu.classList.add('show');
+  openBtnMenu.style.display = 'none';
+  closeBtnMenu.style.display = 'block';
+});
+
+closeBtnMenu.addEventListener('click', function() {
+  sideMenu.classList.remove('show');
+  closeBtnMenu.style.display = 'none';
+  openBtnMenu.style.display = 'block';
+});
+
+/* Activating content links */
+
+const contentLinks = document.querySelectorAll('.content ol li a');
+
+for (let contentLink of contentLinks) {
+  contentLink.addEventListener('click', function() {
+    contentLink.classList.add('active');
+  });
+}
